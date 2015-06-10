a54ecfa7ad5b78300fdc0798de6701a1169e6d5a
Caroline Karlsen <caroline.karlsen53@gmail.com>
2015-06-10 15:24:06 +0200

    Created a file that will contain the urls for the useraccounts app

---
+++ lemon/useraccounts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+from useraccounts import views
+
+urlpatterns = patterns('',
+	url(r'^register$', views.user_register, name='user_register'),
+	url(r'^edit_profile$', views.user_edit_profile, name='user_edit_profile'),
+	url(r'^login$', views.user_login, name='user_login'),
+	url(r'^logout$', views.user_logout, name='user_logout'),
+	url(r'^user_profile$', views.user_profile, name='user_profile'),
+	)
